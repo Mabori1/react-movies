ce67cf8e96f0c32e9925e1cac73b491d4b836e8d
Alexey Makeev <makeevhome@mail.ru>
2022-09-23 13:23:58 +0300

    2

---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" classNameName="brand-logo">
+          React Movies
+        </a>
+        <ul id="nav-mobile" classNameName="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
