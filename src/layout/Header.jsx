c60571e6bb941e0e814cb987bc0ecbad552f5b37
Alexey Makeev <makeevhome@mail.ru>
2022-09-23 23:06:55 +0300

    7

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="#69f0ae green accent-2">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="!#" className="rand-logo">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
